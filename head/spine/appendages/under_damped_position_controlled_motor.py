c4fc6724dcf3b14f5ca97eb979e851e61177314f
Andrew <amessing@utk.edu>
2017-03-31 13:31:30 +0000

    udpcm

--- head/spine/appendages/under_damped_position_controlled_motor.py
+++ head/spine/appendages/under_damped_position_controlled_motor.py
@@ -2,14 +2,20 @@ from .component import Component
 from ...units import *
 
 
-class PositionControlledMotor(Component):
-    SET_VOLTAGE = "kSetPCMVoltage"
-    SET_POSITION = "kSetPCMPosition"
-    GET_POSITION = "kGetPCMPosition"
-    GET_POSITION_RESULT = "kGetPCMPositionResult"
-    GET_VELOCITY = "kGetPCMVelocity"
-    GET_VELOCITY_RESULT = "kGetPCMVelocityResult"
-    STOP = "kStopPCM"
+class UnderDampedPositionControlledMotor(Component):
+    SET_VOLTAGE = "kSetUDPCMVoltage"
+    SET_POSITION = "kSetUDPCMPosition"
+    SET_ALLOWED_DIRECTION = "UDPCMkSetAllowedDirection"
+    GET_POSITION = "kGetUDPCMPosition"
+    GET_POSITION_RESULT = "kGetUDPCMPositionResult"
+    GET_VELOCITY = "kGetUDPCMVelocity"
+    GET_VELOCITY_RESULT = "kGetUDPCMVelocityResult"
+    STOP = "kStopUDPCM"
+
+    # for allowed direction
+    DIRECT = 0
+    FORWARD = DIRECT
+    REVERSE = 1
 
     def __init__(self, spine, devname, config, commands, sim):
         self.spine = spine
@@ -27,6 +33,7 @@ class PositionControlledMotor(Component):
         if not self.sim:
             self.setVoltageIndex = commands[self.SET_VOLTAGE]
             self.setPositionIndex = commands[self.SET_POSITION]
+            self.setAllowedDirectionIndex = commands[self.SET_ALLOWED_DIRECTION]
             self.getPositionIndex = commands[self.GET_POSITION]
             self.getPositionResultIndex = commands[self.GET_POSITION_RESULT]
             self.getVelocityIndex = commands[self.GET_VELOCITY]
@@ -36,6 +43,7 @@ class PositionControlledMotor(Component):
     def get_command_parameters(self):
         yield self.setVoltageIndex, [self.SET_VOLTAGE, "ii"]
         yield self.setPositionIndex, [self.SET_POSITION, "id"]
+        yield self.setAllowedDirectionIndex, [self.SET_ALLOWED_DIRECTION, "ii"]
         yield self.getPositionIndex, [self.GET_POSITION, "i"]
         yield self.getPositionResultIndex, [self.GET_POSITION_RESULT, "d"]
         yield self.getVelocityIndex, [self.GET_VELOCITY, "i"]
@@ -47,6 +55,9 @@ class PositionControlledMotor(Component):
 
     def set_position(self, position):
         self.spine.send(self.devname, False, self.SET_POSITION, self.index, position)
+
+    def set_allowed_direction(self, direction):
+        self.spine.send(self.devname, False, self.SET_ALLOWED_DIRECTION, self.index, direction)
 
     def get_position(self):
         response = self.spine.send(self.devname, True, self.GET_POSITION, self.index)
