58eb2cbc9fd1b5da79095024f7211025ee7a5a26
Andrew Messing <amessing@utk.edu>
2017-04-05 12:42:15 -0400

    Modified state machine to work with dominate

--- head/spine/fsm/state_machine.py
+++ head/spine/fsm/state_machine.py
@@ -5,6 +5,7 @@ class StateMachine(object):
     def __init__(self):
         self.create_state_file()
         self.current_state = None
+        self.state_variables = {}
 
     def create_state_file(self):
         program = os.path.basename(__file__)[:-3]  # remove .py
@@ -21,11 +22,13 @@ class StateMachine(object):
         # file exists
         with open(self.state_file) as f:
             text = f.read()
-        return State(json.loads(text))
+        self.state_variables = json.loads(text)
 
-    def save_state(self, state):
+        #TODO: load state using black magic
+
+    def save_state(self):
         with open(self.state_file, 'w') as f:
-            f.write(state.to_json())
+            f.write(json.dumps(self.state_variables, sort_keys=True, indent=4))
 
     def remove_state_file(self):
         os.remove(self.state_file)
@@ -33,20 +36,24 @@ class StateMachine(object):
     def set_state(self, state):
         self.current_state = state
 
-    def start(self):
+    def start(self, state_variables):
         if self.current_state is None:
             raise NoStateException()
 
+        if self.state_variables is None:
+            self.state_variables = state_variables
+
         while True:
-            self.current_state.run()
-            self.current_state = self.current_state.next_state()
+            self.state_variables.update(self.current_state.run())
+            self.current_state = self.current_state.next_state(self.state_variables)
             if self.current_state is None:
                 break
 
     def __setattr__(self, k, v):
         # on state change save
         if k == 'current_state':
-            self.save_state(v)
+            self.state_variables['current_state'] = v.name
+            self.save_state()
 
         # Normal
         self.__dict__[k] = v
